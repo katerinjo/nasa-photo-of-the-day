bc9d15e6a11f1dfae07fefc6025c6a8afa9dbdda
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-17 13:59:02 -0700

    can now read from input element

--- src/components/TimeMachine/TimeMachine.js
+++ src/components/TimeMachine/TimeMachine.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function TimeMachine({ setDate }) {
-    return <input type="text"></input>;
+    function validDate(plainText) {
+        return plainText === "2019-7-15";
+    }
+
+    function tryUpdate(event) {
+        console.log(event);
+    }
+
+    return <input type="text" onChange={event => tryUpdate(event.target.value)} />;
 }
