81c46012c477ced580aa5dc7e2c585225f823579
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-17 13:04:40 -0700

    install axios; WIP: state variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SpacePanel from "./components/SpacePanel/SpacePanel.js";
 import TimeButton from "./components/TimeButton/TimeButton.js";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [date, setDate] = useState();
+
+  useEffect(() => {
+    axios.get('');
+  });
+
   return (
     <div className="App">
       <p>
