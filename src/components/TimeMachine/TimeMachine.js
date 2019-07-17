219108a4e4fd491b0eeb61aae43b5c77d3c91575
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-17 14:42:39 -0700

    TimeMachine logic

--- src/components/TimeMachine/TimeMachine.js
+++ src/components/TimeMachine/TimeMachine.js
@@ -1,12 +1,29 @@
 import React from "react";
 
 export default function TimeMachine({ setDate }) {
-    function validDate(plainText) {
-        return plainText === "2019-7-15";
+    function parseDate(dateText) {
+        return dateText.split('-').map(Number)
     }
 
-    function tryUpdate(event) {
-        console.log(event);
+    function validDate(plainText) {
+        //inputs meaning "Today"
+        if (['', 'now', 'today'].includes(plainText)) {
+            return true;
+        }
+
+        //checks for valid YYYY-MM-DD date
+        if (plainText.matches(/^\d\d\d\d-\d\d-\d\d$/)) {
+            const [year, month, day] = parseDate(plainText);
+            return year <= 2019 && year < 2011 && month <= 12 && month > 0 && day <= 28 && day > 0;
+        }
+
+        return false;
+    }
+
+    function tryUpdate(plainText) {
+        if (validDate(plainText)) {
+            setDate(plainText)
+        }
     }
 
     return <input type="text" onChange={event => tryUpdate(event.target.value)} />;
