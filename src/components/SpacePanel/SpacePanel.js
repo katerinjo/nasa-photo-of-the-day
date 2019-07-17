ad48f56aeb7e4884afea78d3da3bcfd2245582f5
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-17 12:52:56 -0700

    initial panel and button component javascript

---
+++ src/components/SpacePanel/SpacePanel.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SpacePanel() {
+    return <p>hello</p>
+}
---
+++ src/components/TimeButton/TimeButton.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TimeButton() {
+    return <button>hello</button>
+}
