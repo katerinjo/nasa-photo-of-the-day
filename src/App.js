dad847f1678aab32c5791175b42fe1c95d58b7c4
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-18 12:41:02 -0700

    fixed: image loaded on startup

--- src/App.js
+++ src/App.js
@@ -6,34 +6,48 @@ import "./App.css";
 
 function App() {
   const [explanation, setExplanation] = useState("Loading...")
-  const [pendingDate, setPendingDate] = useState(null);
+  const [pendingDate, setPendingDate] = useState('');
   const [date, setDate] = useState(null);
   const [imgUrl, setImgUrl] = useState(null);
+  const [axiosCount, setAxiosCount] = useState(0);
+
+  function fetchData(date) {
+    const dateQuery = date ? `&date=${date}` : '';
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY${dateQuery}`)
+      .then(response => {
+        console.log('response:', response);
+        setImgUrl(response.data.url);
+        setExplanation(response.data.explanation);
+        setAxiosCount(axiosCount + 1);
+      })
+      .catch(console.log);
+  }
+
+  function fetchDataOther() {
+    axios
+      .get(`https://dog.ceo/api/breeds/image/random`)
+      .then(response => {
+        console.log('response:', response);
+        setImgUrl(response.data.message);
+        const newCount = axiosCount + 1;
+        setAxiosCount(newCount);
+        setExplanation(`axios count: ${newCount}`);
+      })
+      .catch(console.log);
+  }
 
   useEffect(() => {
-    function fetchData(date) {
-      const dateQuery = date ? `&date=${date}` : '';
-      axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY${dateQuery}`)
-        .then(response => {
-          console.log('response:', response);
-          setImgUrl(response.data.url);
-          setExplanation(response.data.explanation);
-        })
-        .catch(console.log);
-    }
+    console.log('useEffect called');
+    console.log('date', date, 'pendingDate', pendingDate);
     if (pendingDate !== date) {
-      fetchData(pendingDate);
+      fetchDataOther(pendingDate);
       setDate(pendingDate);
     }
   }, [pendingDate]);
 
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
       <h1>NASA Daily Photos</h1>
       <SpacePanel
         imgUrl={imgUrl}
