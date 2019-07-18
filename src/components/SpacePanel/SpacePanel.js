7b1e1d1fdd0c452fb9d4dbdf465cbb7e8c5fdeef
Catherine Stewart <catherine.c.stwrt@gmail.com>
2019-07-18 13:06:44 -0700

    use semantic-ui for SpacePanel

--- src/components/SpacePanel/SpacePanel.js
+++ src/components/SpacePanel/SpacePanel.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Card, Image } from "semantic-ui-react";
 
 export default function SpacePanel({ date, imgUrl, explanation }) {
     return (
-        <div>
-            <h2>Date: {date || "Today"}</h2>
-            <img src={imgUrl} alt="regarding the glories of space" />
-            <p>{explanation || "[no explanation available]"}</p>
-        </div>
+        // <div>
+        //     <h2>Date: {date || "Today"}</h2>
+        //     <img src={imgUrl} alt="regarding the glories of space" />
+        //     <p>{explanation || "[no explanation available]"}</p>
+        // </div>
+        <Card>
+            <Image src={imgUrl} alt="regarding the glories of space" />
+            <Card.Content>
+                <Card.Header>Date: {date || "Today"}</Card.Header>
+                <Card.Description>
+                    {explanation || "[no explanation available]"}
+                </Card.Description>
+            </Card.Content>
+        </Card>
     );
 }
